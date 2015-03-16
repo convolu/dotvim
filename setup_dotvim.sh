05f714239ca612a3531315161b1aa91e8a691c5b
Costas Voloudakis <cvoloudakis@gmail.com>
2015-03-16 23:32:26 +0000

    Skip gvim if it is not present

--- setup_dotvim.sh
+++ setup_dotvim.sh
@@ -22,45 +22,46 @@ gvimrc_dest="$HOME/.gvimrc"
 if [ ! -e "$vimrc_src" ]; then
     >&2 echo "File $vimrc_src does not exist"
     exit 1
+else
+    if [ -e "$vimrc_dest" ]; then
+        while true; do
+            read -r -p 'A .vimrc file already exists. Overwrite? [y/n] ' ans
+            case "$ans" in
+                y|Y) rm "$vimrc_dest"
+                     break;;
+                n|N) echo "Aborting ..."
+                     exit 2;;
+                *) echo "Not valid, try again";;
+            esac
+        done
+    fi
+
+    ln -s "$vimrc_src" "$vimrc_dest"
+
 fi
+
 
 if [ ! -e "$gvimrc_src" ]; then
     >&2 echo "File $gvimrc_src does not exist"
-    exit 1
+else
+    if [ -e "$gvimrc_dest" ]; then
+        while true; do
+            read -r -p 'A .gvimrc file already exists. Overwrite? [y/n] ' ans
+            case "$ans" in
+                y|Y) rm "$gvimrc_dest"
+                     break;;
+                n|N) echo "Aborting ..."
+                     exit 2;;
+                *) echo "Not valid, try again";;
+            esac
+        done
+    fi
+
+    ln -s "$gvimrc_src" "$gvimrc_dest"
+
 fi
-
-if [ -e "$vimrc_dest" ]; then
-    while true; do
-        read -r -p 'A .vimrc file already exists. Overwrite? [y/n] ' ans
-        case "$ans" in
-            y|Y) rm "$vimrc_dest"
-                 break;;
-            n|N) echo "Aborting ..."
-                 exit 2;;
-            *) echo "Not valid, try again";;
-        esac
-    done
-fi
-
-ln -s "$vimrc_src" "$vimrc_dest"
-
-if [ -e "$gvimrc_dest" ]; then
-    while true; do
-        read -r -p 'A .gvimrc file already exists. Overwrite? [y/n] ' ans
-        case "$ans" in
-            y|Y) rm "$gvimrc_dest"
-                 break;;
-            n|N) echo "Aborting ..."
-                 exit 2;;
-            *) echo "Not valid, try again";;
-        esac
-    done
-fi
-
-ln -s "$gvimrc_src" "$gvimrc_dest"
 
 # Fetch submodules
-
 pushd "$HOME/.vim"
 git submodule init
 git submodule update
